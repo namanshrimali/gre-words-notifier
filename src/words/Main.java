8abe1775a667f9b78098ef3e7df15a4bb7218f68
Naman Shrimali <namanshrimali@gmail.com>
2020-06-19 22:05:51 +0530

    Added level 7 for advanced words

--- src/words/Main.java
+++ src/words/Main.java
@@ -11,7 +11,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("======= Sections =======\n3. Advanced Words\nSelect section : ");
         short section = scanner.nextShort();
-        System.out.print("======= Levels =======\n4. Level 4\n5. Level 5\nSelect level : ");
+        System.out.print("======= Levels =======\n4. Level 4\n5. Level 5\n6. Level 6\n7. Level 7\nSelect level : ");
         short level = scanner.nextShort();
         System.out.print("Enter the desired frequency (words/minute) in which you want to see the notifications : ");
         WordsGenerator words = new WordsGenerator(section, level);
